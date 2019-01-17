27e8df4cd171036fe18e6e2378aabb1390365355
anotherLostKitten <tpeters@stuy.edu>
2019-01-17 11:01:52 -0500

    >>>>....

--- client.c
+++ client.c
@@ -13,7 +13,7 @@ int client(int argc, char **argv){
     else
 	server_socket = client_setup( TEST_IP );
     setupSDL();
-    
+    struct keysdown* keys = calloc(sizeof(struct keysdown),1);//NOTE CLIENT SIDE IN FINAL VERSION then it should be written to a pipe, the server should read the struct and execute as of now unimplemented actions based on this.
     while(!quit){    
 	keys->xk=0;
 	keys->zj=0;
@@ -33,6 +33,7 @@ int client(int argc, char **argv){
 		break;
             }
         }
+	// send key struct
 	// get data from server
         render(player,grid,playarray,enemyarray,eqarray);
     }

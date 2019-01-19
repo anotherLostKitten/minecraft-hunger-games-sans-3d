26af27d87396523ffab6752b28ec3e700c18c90c
Theodore Peters <tpeters@stuy.edu>
2019-01-19 11:56:28 -0500

    :

--- pkp.c
+++ pkp.c
@@ -6,10 +6,10 @@
 int process_keypress(struct keysdown* keys,int playernum,struct Grid* grid,struct player* playarray,struct enemy* enemyarray,struct equipment* equarray){
     struct player player=playarray[playernum];
     int xdir=0,ydir=0;
-    if(keys->upw) xdir=0,ydir=-1;
-    if(keys->downs) xdir=0,ydir=1;
-    if(keys->lefta) xdir=-1,ydir=0;
-    if(keys->rightd) xdir=1,ydir=0;
+    if(keys->upw) ydir=-1;
+    if(keys->downs) ydir=1;
+    if(keys->lefta) xdir=-1;
+    if(keys->rightd) xdir=1;
     for(int i = 0;i<NUM_PLAYERS;i++){
         if(i!=playernum && playarray[i].coords[0]==player.coords[0] + ydir && playarray[i].coords[1]==player.coords[1] + xdir){
             attack(player,playarray[i]);

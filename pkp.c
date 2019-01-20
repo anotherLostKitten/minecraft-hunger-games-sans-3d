fefb652e1ae212d03cb63b4c31202a1f5203928b
jesings <jsinger@stuy.edu>
2019-01-19 19:15:39 -0500

    Okay added rendering on the side and fixed equipment`

--- pkp.c
+++ pkp.c
@@ -5,7 +5,7 @@
 #include "enemy.h"
 #include "atk.h"
 int process_keypress(struct keysdown* keys,int playernum,struct Grid* grid,struct player* playarray,struct enemy* enemyarray,struct equipment* equarray){
-    struct player player=playarray[playernum];
+    struct player* player=playarray+playernum;
     int xdir=0,ydir=0;
     if(keys->upw) ydir=-1;
     if(keys->downs) ydir=1;
@@ -13,14 +13,14 @@ int process_keypress(struct keysdown* keys,int playernum,struct Grid* grid,struc
     if(keys->rightd) xdir=1;
     //printf("processing keypress %d %d\n",xdir,ydir);
     for(int i = 0;i<NUM_PLAYERS;i++){
-        if(i!=playernum && playarray[i].coords[0]==player.coords[0] + ydir && playarray[i].coords[1]==player.coords[1] + xdir){
-            attack(player,playarray[i]);
+        if(i!=playernum && playarray[i].coords[0]==player->coords[0] + ydir && playarray[i].coords[1]==player->coords[1] + xdir){
+            attack(player,playarray+i);
             goto aftermove;
         }
     }
     for(int i = 0;i<MAXENMY;i++){
-        if(enemyarray[i].coords[0]==player.coords[0] + ydir && enemyarray[i].coords[1]==player.coords[1] + xdir){
-            attackE(player,enemyarray[i]);
+        if(enemyarray[i].coords[0]==player->coords[0] + ydir && enemyarray[i].coords[1]==player->coords[1] + xdir){
+            attackE(player,enemyarray+i);
             goto aftermove;
         }
     }
@@ -32,12 +32,12 @@ aftermove:
     if(keys->zj) ;
     if(keys->xk){
         for(int i = 0;i<MAXEQ;i++){
-            if(equarray[i].coords[0]==player.coords[0] && equarray[i].coords[1]==player.coords[1]){
-                if(player.equipment[equarray[i].typeindex]+1){
-                    equarray[player.equipment[equarray[i].typeindex]].coords[0]=player.coords[0];
-                    equarray[player.equipment[equarray[i].typeindex]].coords[1]=player.coords[1];
+            if(equarray[i].coords[0]==player->coords[0] && equarray[i].coords[1]==player->coords[1]){
+                if(player->equipment[equarray[i].typeindex]+1){
+                    equarray[player->equipment[equarray[i].typeindex]].coords[0]=player->coords[0];
+                    equarray[player->equipment[equarray[i].typeindex]].coords[1]=player->coords[1];
                 }
-                player.equipment[equarray[i].typeindex] = i;
+                player->equipment[equarray[i].typeindex] = i;
                 equarray[i].coords[0]=-1;
                 equarray[i].coords[1]=-1;
                 return 0;
